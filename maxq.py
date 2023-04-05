e3d7ca902a0a3750f643d705da20561b623cd284
Hamid Osooli <hamid.osooli@gmail.com>
2023-04-05 19:30:53 -0400

    initial commit

---
+++ maxq.py
@@ -0,0 +1,66 @@
+import gym
+
+class Skill:
+    def __init__(self, policy, id):
+        self.policy = policy
+        self.id = id
+
+class CompositeTask:
+    def __init__(self, skills, id):
+        self.skills = skills
+        self.id = id
+
+    def subtask(self, state):
+        raise NotImplementedError
+
+    def act(self, state):
+        skill = self.skills[self.subtask(state)]
+        return skill.policy(state)
+
+class CartPoleTask(CompositeTask):
+    def __init__(self, skills, id):
+        super().__init__(skills, id)
+
+    def subtask(self, state):
+        if state[2] < 0:
+            return 0
+        else:
+            return 1
+
+class MAXQAgent:
+    def __init__(self, tasks, root):
+        self.tasks = tasks
+        self.current_task = root
+
+    def act(self, state):
+        while isinstance(self.current_task, CompositeTask):
+            subtask = self.current_task.subtask(state)
+            self.current_task = self.tasks[self.current_task.id].skills[subtask]
+
+        return self.current_task.policy(state)
+
+# Example usage
+env = gym.make('CartPole-v1', render_mode="rgb_array")
+skills = [
+    Skill(lambda s: 1, 0),
+    Skill(lambda s: 0, 1),
+]
+tasks = [
+    CartPoleTask(skills, 0)
+]
+agent = MAXQAgent(tasks, tasks[0])
+
+for i_episode in range(20):
+    observation = env.reset()[0]
+
+    for t in range(100):
+        env.render()
+        action = agent.act(observation)
+        observation, reward, done, truncated, info = env.step(action)
+
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            agent.current_task = tasks[0]
+            break
+
+env.close()
---
+++ options.py
@@ -0,0 +1,41 @@
+import numpy as np
+import gym
+
+
+class Option:
+    def __init__(self, policy, termination, id):
+        self.policy = policy
+        self.termination = termination
+        self.id = id
+
+
+class OptionAgent:
+    def __init__(self, options):
+        self.options = options
+        self.current_option = None
+
+    def act(self, state):
+        if self.current_option is None or self.current_option.termination(state):
+            self.current_option = np.random.choice(self.options)
+        return self.current_option.policy(state)
+
+
+# Example usage
+env = gym.make('CartPole-v1', render_mode="rgb_array")
+options = [
+    Option(lambda s: 1 if s[2] < 0 else 0, lambda s: s[2] >= 0, 0),
+    Option(lambda s: 1 if s[2] >= 0 else 0, lambda s: s[2] < 0, 1)
+]
+agent = OptionAgent(options)
+for i_episode in range(20):
+    observation = env.reset()[0]
+
+    for t in range(100):
+        env.render()
+        action = agent.act(observation)
+        observation, reward, done, truncated, info = env.step(action)
+
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
+env.close()
